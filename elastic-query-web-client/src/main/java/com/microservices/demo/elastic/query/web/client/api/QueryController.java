3a4fe7790e77ba98a6e5cc5fd9c6d39da0bb37c4
Frank Hundley <mariner105@msn.com>
2021-11-14 19:29:50 +0000

    Added keycloak

--- elastic-query-web-client/src/main/java/com/microservices/demo/elastic/query/web/client/api/QueryController.java
+++ elastic-query-web-client/src/main/java/com/microservices/demo/elastic/query/web/client/api/QueryController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
